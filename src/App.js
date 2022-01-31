cfeed8a040151be626929f5d50a243bf841ad2b2
Irina Sabaliauskiene <sabaliauskieneirina@gmail.com>
2022-01-31 15:22:21 +0100

    main

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Profile from "./components/Profile/Profile.jsx";
+import data from "./data/data.json";
+import friends from "./data/friends.json";
+import user from "./data/user.json";
+import transactions from "./data/transactions.json";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Profile
+        avatar={user.avatar}
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        stats={user.stats}
+      />
+      {/* <Friendlist friends={friends} />
+    <Statistics data={data} />
+    <TransactionHistory transactions={transactions}/> */}
+    </>
   );
 }
 
